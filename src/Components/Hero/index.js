f4ed88b554a220125b4579a238a7e9f2af4141a3
415CA <chrisallen415@gmail.com>
2023-12-13 14:30:49 -0500

    Reverse Social icon size

--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -12,7 +12,7 @@ const Hero = () => (
           Project Management | Full Stack Web Development | Media Licensing | Advertising
         </h2>
         <HomepageTech />
-        <Social color='#FFFFFF' size={51} />
+        <Social color='#FFFFFF' size={50} />
       </div>
     </div>
   </div>
